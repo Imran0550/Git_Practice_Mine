96ec407115a1d69bcf3db3a2b3d3df6c8dff9123
sayidimrannazari <Sayidi_n@yahoo.com>
2021-10-04 17:07:03 -0400

    added food file

---
+++ src/PracOne/Food.java
@@ -0,0 +1,11 @@
+package PracOne;
+
+public class Food {
+
+    public static void main(String[] args) {
+
+        System.out.println("Are you hungary");
+
+        System.out.println("yes");
+    }
+}
